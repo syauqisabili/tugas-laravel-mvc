6ec0fe2745b8874bd057a9b4d055a81327dbd305
syauqisabili <syauqisabili@gmail.com>
2020-07-01 09:58:48 +0700

    merubah function welcome di AuthController.php me-return request dalam bentuk object

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     public function welcome(Request $req){
-
-        return view('greet',['firstname' => $req['firstname'], 'lastname' => $req['lastname'] ]);
+        return view('greet', ['req' => $req] );
+        //return view('greet',['firstname' => $req['firstname'], 'lastname' => $req['lastname'] ]);
     }
 }
